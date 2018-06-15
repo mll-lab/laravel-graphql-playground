6a8af2c7931bbd8be418c55435fb3587fc2274cc
spawnia <benedikt@franke.tech>
2018-06-15 16:52:54 +0200

    Use php-cs-fixer

--- src/GraphQLPlaygroundServiceProvider.php
+++ src/GraphQLPlaygroundServiceProvider.php
@@ -2,14 +2,13 @@
 
 namespace MLL\GraphQLPlaygroundServiceProvider;
 
-use GraphQLPlayground\Console\PublishCommand;
 use Illuminate\Support\ServiceProvider;
 
 class GraphQLPlaygroundServiceProvider extends ServiceProvider
 {
     const CONFIG_PATH = __DIR__ . '/../config/grapqhl-playground.php';
     const VIEW_PATH = __DIR__ . '/../resources/views';
-    
+
     /**
      * Perform post-registration booting of services.
      *
@@ -18,20 +17,20 @@ class GraphQLPlaygroundServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadViewsFrom(self::VIEW_PATH, 'graphiql');
-        
+
         $this->publishes([
             self::CONFIG_PATH => config_path('graphql-playground.php'),
         ], 'config');
-        
+
         $this->publishes([
             self::VIEW_PATH => resource_path('views/vendor/graphql-playground'),
         ], 'views');
-        
-        \Route::get(config('graphql-playground.frontend'), function (){
+
+        \Route::get(config('graphql-playground.frontend'), function () {
             return view('graphql-playground::index');
         });
     }
-    
+
     /**
      * Register bindings in the container.
      *
