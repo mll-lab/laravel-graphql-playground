acc5e80316aee488b6c008b3e4184acfd25b8187
Benedikt Franke <benedikt.franke@mll.com>
2018-08-20 09:43:03 +0200

    Add option to download assets to serve them locally

---
+++ src/DownloadAssetsCommand.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace MLL\GraphQLPlayground;
+
+use Illuminate\Console\Command;
+
+class DownloadAssetsCommand extends Command
+{
+    const JS_PATH_LOCAL = 'vendor/graphql-playground/middleware.js';
+    const JS_PATH_CDN = '//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js';
+    const CSS_PATH_LOCAL = 'vendor/graphql-playground/index.css';
+    const CSS_PATH_CDN = '//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css';
+    const FAVICON_PATH_LOCAL = 'vendor/graphql-playground/favicon.png';
+    const FAVICON_PATH_CDN = '//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png';
+    
+    protected $signature = 'graphql-playground:download-assets';
+    protected $description = 'Download the newest version of the GraphQLPlayground assets to serve them locally.';
+    
+    public function handle()
+    {
+        $this->fileForceContents(
+            public_path(self::CSS_PATH_LOCAL),
+            file_get_contents('https:' . self::CSS_PATH_CDN)
+        );
+        $this->fileForceContents(
+            public_path(self::JS_PATH_LOCAL),
+            file_get_contents('https:' . self::JS_PATH_CDN)
+        );
+        $this->fileForceContents(
+            public_path(self::FAVICON_PATH_LOCAL),
+            file_get_contents('https:' . self::FAVICON_PATH_CDN)
+        );
+    }
+    
+    protected function fileForceContents(string $dir, string $contents)
+    {
+        $parts = explode('/', $dir);
+        $file = array_pop($parts);
+        $dir = '';
+        foreach ($parts as $part) {
+            if (!is_dir($dir .= "/$part")) {
+                mkdir($dir);
+            }
+        }
+        file_put_contents("$dir/$file", $contents);
+    }
+}
