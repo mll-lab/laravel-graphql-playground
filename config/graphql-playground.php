f378f6ff20b5ea140c7c06aa4220291fb67931f2
Danijel Dedic <dedic.d@gmail.com>
2019-02-22 11:35:24 +0100

    Route comment

--- config/graphql-playground.php
+++ config/graphql-playground.php
@@ -22,12 +22,6 @@ return [
     |
     | Additional configuration for the route group https://lumen.laravel.com/docs/routing#route-groups
     |
-    | Beware that middleware defined here runs before the GraphQL execution phase.
-    | This means that errors will cause the whole query to abort and return a
-    | response that is not spec-compliant. It is preferable to use directives
-    | to add middleware to single fields in the schema.
-    | Read more https://lighthouse-php.com/docs/auth.html#apply-auth-middleware
-    |
     */
 
     'route' => [
