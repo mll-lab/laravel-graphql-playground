8f08023b54dd483a3e8369a8562c333b16656250
Benedikt Franke <benedikt@franke.tech>
2019-04-17 18:35:57 +0200

    Update graphql-playground.php

--- config/graphql-playground.php
+++ config/graphql-playground.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 return [
     /*
     |--------------------------------------------------------------------------
-    | GraphQL Playground endpoint
+    | GraphQL Playground route
     |--------------------------------------------------------------------------
     |
-    | Set the endpoint to which the GraphQL Playground responds.
-    | The default route endpoint is "yourdomain.com/graphql-playground".
+    | Set the route to which the GraphQL Playground responds.
+    | The default endpoint is "yourdomain.com/graphql-playground".
     |
     */
 
@@ -24,6 +24,7 @@ return [
     |
     | For domain config it could be something like:
     | 'domain' => 'graphql.' . env('APP_DOMAIN', 'localhost'),
+    |
     */
 
     'route' => [
@@ -34,8 +35,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Route for the GraphQL endpoint
+    | Default GraphQL endpoint
     |--------------------------------------------------------------------------
+    |
+    | The default endpoint that the Playground UI is set to.
+    | It assumes you are running GraphQL from the same Laravel instance,
+    | but can be set to any URL.
     |
     */
 
@@ -43,11 +48,12 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Route for the GraphQL endpoint
+    | Control Playground availibility
     |--------------------------------------------------------------------------
     |
-    | Control if the playground is accessible at all
-    | This allows you to disable it completely in production
+    | Control if the playground is accessible at all.
+    | This allows you to disable it in certain environments,
+    | for example you might not want it active in production.
     |
     */
 
