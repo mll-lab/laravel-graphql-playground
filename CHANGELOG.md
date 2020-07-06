e8527486485676a2c91eb9ac4a790c54c621822d
spawnia <benedikt@franke.tech>
2020-07-06 21:28:28 +0200

    Fix loading removal by removing remaining bits

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+## v2.2.1
+
+### Fixed
+
+- Fix loading removal by removing remaining bits
+
 ## v2.2.0
 
 ### Changed
--- views/index.blade.php
+++ views/index.blade.php
@@ -21,11 +21,7 @@
 <div id="root"/>
 <script type="text/javascript">
     window.addEventListener('load', function (event) {
-        const loadingWrapper = document.getElementById('loading-wrapper');
-        loadingWrapper.classList.add('fadeOut');
-
         const root = document.getElementById('root');
-        root.classList.add('playgroundIn');
 
         GraphQLPlayground.init(root, {
             endpoint: "{{url(config('graphql-playground.endpoint'))}}"
