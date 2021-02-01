55183326c44b2898fdf23b784e2991e2a4006388
Benedikt Franke <benedikt@franke.tech>
2021-02-01 18:51:24 +0100

    Update views/index.blade.php

--- views/index.blade.php
+++ views/index.blade.php
@@ -24,8 +24,8 @@
         const root = document.getElementById('root');
 
         GraphQLPlayground.init(root, {
+            endpoint: "{{url(config('graphql-playground.endpoint'))}}",
             subscriptionEndpoint: "{{config('graphql-playground.subscriptionEndpoint')}}",
-            endpoint: "{{url(config('graphql-playground.endpoint'))}}"
         })
     })
 </script>
